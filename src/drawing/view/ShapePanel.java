082465a2e6693f62afb5ba02fe8727c8b817ed4a
Colm Laro <smartman97@gmail.com>
2016-04-22 09:50:36 -0600

    Begging the shapePanel

--- src/drawing/view/ShapePanel.java
+++ src/drawing/view/ShapePanel.java
@@ -1,13 +1,30 @@
 package drawing.view;
 
-import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.util.ArrayList;
 
-import drawing.controller.DrawingController;
+import javax.swing.JPanel;
 
 public class ShapePanel extends JPanel
 {
-	public ShapePanel(DrawingController baseController)
+	private ArrayList<Rectangle> rectangleList;
+	//More arraylists
+	
+	public ShapePanel()
 	{
-		
+		rectangleList = new ArrayList<Rectangle>();
+	}
+	
+	public void addRectangle()
+	{
+		//Random info here
+		rectangleList.add(new Rectangle(10, 10, 10 , 10));
+	}
+	
+	@Override
+	protected void paintComponent(Graphics currentGraphics)
+	{
+		//Draw in here!
 	}
 }
